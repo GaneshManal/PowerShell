085f916d8412688d91e9feaba5f8ebb48441a44e
GaneshManal <ganesh.manal@maplelabs.com>
2021-09-21 23:47:20 +0530

    first draft - hellow world & find files with string ( grep -l )

---
+++ Get-String.ps1
@@ -0,0 +1,33 @@
+﻿#$InputPath = "D:\Git\PowerShell"
+#$String = "*Password*"
+
+Function Get-FileswithString
+{
+
+    param (
+    $InputPath,
+    $String
+    )
+
+    $AllLogFiles = Get-ChildItem -Path $InputPath -Filter "*.log"
+
+    Foreach ($OneFile in $AllLogFiles)
+    {
+    $FileContent = $null
+    $FileContent = Get-Content -Path $OneFile.FullName
+
+        If($FileContent -like $String)
+        {
+            #Write-Host "Found" -ForegroundColor Green
+            Write-Host "[     Found ] " -F Green -NoNewline
+            Write-Host $OneFile.FullName
+        }
+        Else
+        {
+            Write-Host "[ Not Found ] " -F Red -NoNewline
+            Write-Host $OneFile.FullName
+        }
+    }
+}
+
+Get-FileswithString -InputPath "D:\Git\PowerShell" -String "*Password*"
